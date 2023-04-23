4695f742077161abb6a2763bea20e8ebe7e7037c
vytas <vytasstro@gmail.com>
2023-04-24 00:31:22 +0300

    js and css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import 'bootstrap/dist/js/bootstrap.js';
+
+$(function () {
+    $(document).on('click', '#logout-btn', function (e) {
+        e.preventDefault();
+        $('#logout-form').trigger('submit');
+    });
+});
