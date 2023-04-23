62962f087c5d7134ff17edbf9fecfc60e307aa93
vytas <vytasstro@gmail.com>
2023-04-23 23:33:41 +0300

    Web views

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
+use App\Http\Controllers\ArticlesController;
+use App\Http\Controllers\ConferenceController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Http\Request as HttpRequest;
 use Illuminate\Support\Facades\Route;
+use Spatie\LaravelIgnition\Http\Requests\UpdateConfigRequest;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/contact', [HomeController::class, 'contact'])->name('home.contact');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('conferences', ConferenceController::class)->only(['index', 'show', 'create', 'store', 'edit', 'update']);
+Route::get('conferences/create', [ConferenceController::class, 'create'])->name('conferences.create');
+Route::post('conferences/store', [ConferenceController::class, 'store'])->name('conferences.store');
